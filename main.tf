a30cd624acf50a16fe19f360c7cdfe639ad8017e
tmahicharan <tmahicharan@gmail.com>
2026-08-10 19:42:34 +0530

    infra

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "main"{
     
     local.common_tags,
     {
-        Name= "${local.common_name}-{var.component}"
+        Name= "${local.common_name}-${var.component}"
     }
   )
 }
@@ -51,7 +51,7 @@ resource "aws_ec2_instance_state" "main" {
 
 # Taking component image
 resource "aws_ami_from_instance" "main" {
-  name               = "${local.common_name}-{var.component}-ami"
+  name               = "${local.common_name}-${var.component}-ami"
   source_instance_id = aws_instance.main.id
   depends_on = [aws_ec2_instance_state.main]
   tags = merge (
